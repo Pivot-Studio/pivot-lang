baa924ebaff09828577a770ce30b8d7aa7e92019
2038975825@qq.com <2038975825@qq.com>
2022-09-13 22:49:35 +0800

    feat: keyword gen with macro

--- src/lexer/types.rs
+++ src/lexer/types.rs
@@ -1,6 +1,9 @@
+use lazy_static::lazy_static;
+use std::collections::HashMap;
 #[derive(Debug, PartialEq)]
 pub enum Token {
     Operator(Operator), // 运算符
+    Keyword(Keyword),   // 关键字
     LPAREN,             // (
     RPAREN,             // )
     EOF,                //EOF
@@ -11,4 +14,31 @@ pub enum Operator {
     MINUS, // -
     MUL,   // *
     DIV,   // /
+}
+macro_rules! define_keywords {
+    ($(
+        $ident:ident = $string_keyword:expr
+    ),*) => {
+        #[derive(Debug, PartialEq,Eq,Hash)]
+        pub enum Keyword {
+            NoKeyword,
+            $($ident),*
+        }
+        $(pub const $ident: &'static str = $string_keyword;)*
+        lazy_static! {
+            static ref KEYWORDS_MAP: HashMap<&'static str,Keyword> = {
+                let mut mp = HashMap::new();
+                $(mp.insert($ident,Keyword::$ident);)*
+                mp
+            };
+        }
+    };
+}
+define_keywords!(TESTA = "testa", TESTB = "testb");
+#[test]
+fn test_keyword_gen() {
+    println!(
+        "keyword_map: {:?} {:?}",
+        KEYWORDS_MAP["testa"], KEYWORDS_MAP["testb"]
+    );
 }
