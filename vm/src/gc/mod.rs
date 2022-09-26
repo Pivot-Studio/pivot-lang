27e58c321a69f033513df0293df06025eadc9ed9
bobxli <bobxli@tencent.com>
2022-09-26 10:11:56 +0800

    feat: basic gc

--- vm/src/gc/mod.rs
+++ vm/src/gc/mod.rs
@@ -1,7 +1,173 @@
-struct header<'a> {
-    size: u64,
-    next: &'a header<'a>,
+use std::collections::{HashMap, HashSet};
+
+use add_symbol::is_runtime;
+use libc::{c_void, malloc, memset, size_t};
+
+struct Mem {
+    size: i64,
+    marked: bool,
 }
 
+impl Mem {
+    fn mark(&mut self) {
+        self.marked = true;
+    }
+    fn unmark(&mut self) {
+        self.marked = false;
+    }
+    fn is_marked(&self) -> bool {
+        self.marked
+    }
+}
 
+struct DioGC {
+    memtable: HashMap<*mut c_void, Mem>,
+    size: i64,
+    roots: HashSet<*mut c_void>,
+}
 
+#[is_runtime]
+impl DioGC {
+    pub fn new() -> DioGC {
+        DioGC {
+            size: 0,
+            memtable: HashMap::new(),
+            roots: HashSet::new(),
+        }
+    }
+
+    pub unsafe fn malloc(&mut self, size: i64) -> *mut c_void {
+        let ptr = malloc(size as size_t);
+        memset(ptr, 0, size as size_t);
+        self.memtable.insert(
+            ptr,
+            Mem {
+                size,
+                marked: false,
+            },
+        );
+        self.size += size;
+        ptr
+    }
+
+    pub unsafe fn free(&mut self, ptr: *mut c_void) {
+        if let Some(mem) = self.memtable.remove(&ptr) {
+            self.size -= mem.size;
+        }
+    }
+    pub fn get_size(&self) -> i64 {
+        self.size
+    }
+
+    pub fn add_root(&mut self, ptr: *mut c_void) {
+        self.roots.insert(ptr);
+    }
+
+    pub fn rm_root(&mut self, ptr: *mut c_void) {
+        self.roots.remove(&ptr);
+    }
+
+    pub unsafe fn collect(&mut self) {
+        self.mark();
+        self.sweep();
+    }
+    pub unsafe fn sweep(&mut self) {
+        let mut rm = Vec::new();
+        for (ptr, mem) in self.memtable.iter_mut() {
+            if !mem.marked {
+                rm.push(*ptr);
+            } else {
+                mem.unmark();
+            }
+        }
+        for ptr in rm {
+            self.free(ptr);
+        }
+    }
+
+    pub unsafe fn mark(&mut self) {
+        for ptr in self.roots.iter() {
+            let pointto = *(*ptr as *mut i64);
+            let p = pointto as *mut c_void;
+            Self::mark_ptr(&mut self.memtable, &p);
+        }
+    }
+    pub unsafe fn mark_ptr(memtable: &mut HashMap<*mut c_void, Mem>, ptr: &*mut c_void) {
+        let mem = memtable.get_mut(ptr);
+        if let Some(mem) = mem {
+            if mem.is_marked() {
+                return;
+            }
+            mem.mark();
+            let p = *ptr;
+            let data = p as *mut i64;
+            let size = mem.size / 8;
+            for i in 0..size {
+                let ptr = data.offset(i as isize);
+                let i1 = *ptr;
+                let ptr = i1 as *mut c_void;
+                Self::mark_ptr(memtable, &ptr);
+            }
+        }
+    }
+    pub fn about(&self) {
+        let dio = "
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⡀⠀⠀⠀⠀⠀⠘⠀⣷⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡀⠀⠀⠀⠙⢦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⡼⠃⠀⠀⠀⠀⠀⣤⠀⢏⠀⠀⠀⠀⢠⣠⡆⠀⠀⣦⡀⠀⠳⡀⠀⠀⠀⠀⠑⢄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠐⣇⡀⠀⠀⠀⠀⠀⠘⠂⢈⣦⠀⣀⡀⠈⣟⢷⣄⠀⠘⣷⣄⠀⠹⣆⠀⠀⠀⠀⠀⠙⢦⣀⠀⠀⠀⠀⠀⠀⠀⢤
+        ⠀⠀⠀⠀⠐⣶⠦⠤⠴⠋⠁⠀⠀⠀⠀⡜⢷⣧⣸⣿⡀⡟⠹⡄⢹⠀⣹⣷⣤⡘⣄⠙⠲⢬⣿⣉⡉⠉⠉⠉⠉⢉⣥⣀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠈⠳⠤⢤⡀⠀⠀⠀⠀⠀⢹⡾⣿⠛⠉⣧⡇⠀⢱⣸⡔⢡⠏⠀⠉⢻⣦⣤⠀⠈⠹⣿⣂⡀⣠⠔⢉⡤⠾⡆⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⢀⡞⣧⠀⠀⢠⠈⣇⢀⣿⠃⠀⠀⠸⣿⣠⣼⣟⣠⣯⣴⡿⠷⣿⠟⠁⠀⠀⠀⠀⠀⣇⡇⠀⡿⠦⡀⣇⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⣾⡼⡇⠀⠀⠘⡇⣿⣿⣿⢦⣄⣧⠀⣯⣿⣼⣿⣿⠋⢿⣽⣶⡏⠀⠀⠀⠀⠀⠀⠀⢻⠇⢀⡇⣠⠇⢸⡄⠀⠀⠀⣠
+        ⠀⠀⠀⠀⠀⠀⠀⠙⠓⠳⣤⣶⠀⣿⠛⣿⢻⣷⣮⣽⡆⠈⠿⠟⠻⠛⠉⠉⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠙⠀⠘⢿⠃⠀⣼⠁⠀⠀⠀⡱
+        ⠀⠀⠀⠀⠀⠀⠀⢀⣠⡴⣺⣿⢠⣍⡀⠘⡿⢿⡿⠿⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡈⢀⡾⠃⠀⠀⠀⠘⢄
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠁⢸⡟⣾⡷⣄⢹⠀⠀⠀⣿⠁⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡏⡏⠉⠀⠀⠀⠀⠀⡐⠪
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠃⠈⠃⠀⠙⣇⠀⠀⠙⠦⠉⠉⠁⠀⠀⠀⠀⠀⢠⡆⠀⠀⠀⠀⠀⠀⠀⢸⠃⠹⡄⠀⠀⠀⠀⠀⠠⡀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣆⠀⠀⢠⣤⣤⡤⢒⣊⣩⣽⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⢸⡄⠀⠙⣿⠀⡄⠀⠀⠀⠙
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢦⠀⠈⠹⣶⠛⣩⠔⠋⠉⠁⣸⠀⠀⠀⠀⠀⠀⠀⣠⢞⡁⠀⠀⡞⣸⠃⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠳⣄⠀⠈⣿⣇⣀⣀⣀⢴⡿⠀⠀⠀⠀⠀⣠⠞⠁⣸⠀⢀⡼⠟⠹⡀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⡄⠙⠲⠤⠥⢖⡋⠀⠀⠀⠀⡠⠊⠁⠀⢠⠇⠀⠀⠀⠀⠀⢹⣉⡉⢰⡎
+        ⠀⣀⣤⠖⠒⢲⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣆⠀⠛⠋⠉⠀⠀⢀⡤⠊⠀⠀⠀⠀⠞⠀⠀⠀⠀⠀⠀⠀⢳⡼⠋⠀
+        ⠋⡝⠁⠀⠀⠀⢱⡀⢀⡴⠊⠉⠉⠙⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⢘⣄⣀⣀⣀⡤⠖⠋⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⠤⠤⠖⠊⢁⡠⠖⠋
+        ⠉⠉⠉⠉⠙⡆⠀⢷⠋⠀⠀⢀⡴⠚⠁⠀⠀⠀⠀⠀⠀⣠⠴⣚⠭⠜⠛⢯⠀⡇⠀⠀⣀⣀⠤⠄⠒⠒⠉⠉⠀⣀⣀⠤⠔⠊⠁⠀⠀⠀
+        ⠳⠄⠀⠀⠀⡇⢀⡼⢦⡀⣰⠋⠀⠀⠀⠀⠀⠀⠀⠀⢸⣏⣛⠓⠤⠤⡀⠘⡆⢇⣠⠞⢁⣠⠤⠤⠖⠒⠒⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠈⠀⠀⠀⡟⠋⠀⠀⣹⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⡈⠉⠙⠢⡝⡄⠳⡼⠃⡴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⢀⠀⢀⡴⠃⠀⠀⡸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠇⠀⠀⠀⠙⢸⡞⢠⠞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⣻⠒⠋⠀⠀⠀⡰⠃⠀⠀⠀⠀⠀⠀⠀⣀⣀⠠⠤⠤⠼⡀⠀⠀⠀⠀⡞⢠⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠘⠁⠀⠀⠀⠀⡰⠁⠀⠀⢀⣠⠄⠒⠊⠉⠀⠀⠀⠀⠀⠀⠈⢢⡀⠀⢰⢡⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⢀⣼⣁⠤⠖⠊⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣽⣴⡾⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⢀⣠⠞⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣼⡟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀";
+        println!("{}", dio);
+    }
+}
+
+#[test]
+fn test_basic_gc() {
+    unsafe {
+        let mut gc = DioGC::new();
+        gc.about();
+        // allocate 4 pointers
+        let ptr1 = gc.malloc(64);
+        let ptr2 = gc.malloc(64);
+        let ptr3 = gc.malloc(64);
+        let ptr4 = gc.malloc(64);
+        let size = gc.get_size();
+        // get rust stack pointer point to ptr1
+        let rustptr = &(ptr1 as i64) as *const _ as *mut c_void;
+        assert_eq!(size, 256);
+        gc.add_root(rustptr);
+        // set ptr1 point to ptr2
+        let v1 = ptr2 as i64;
+        *(ptr1 as *mut i64) = v1;
+        gc.collect();
+        assert_eq!(gc.get_size(), 128);
+        // set ptr1 empty
+        *(ptr1 as *mut i64) = 0;
+        gc.collect();
+        assert_eq!(gc.get_size(), 64);
+        // remove gc root
+        gc.rm_root(rustptr);
+        gc.collect();
+        assert_eq!(gc.get_size(), 0);
+    }
+}
