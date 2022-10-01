6f8fb4945c2cb0ba914c0555ec128c382ebcccd9
2038975825@qq.com <2038975825@qq.com>
2022-10-01 15:42:07 +0800

    fix: use hash in test parser

--- internal_macro/src/test_parser_macro/src/lib.rs
+++ internal_macro/src/test_parser_macro/src/lib.rs
@@ -1,58 +1,64 @@
 use proc_macro::TokenStream;
 use quote::{__private::Literal, format_ident, quote};
+use std::{
+    collections::hash_map::DefaultHasher,
+    hash::{Hash, Hasher},
+};
 use syn::{parse_macro_input, ItemFn};
 #[proc_macro_attribute]
 pub fn test_parser(args: TokenStream, input: TokenStream) -> TokenStream {
-    static mut I: i32 = 0;
     let ast = parse_macro_input!(input as ItemFn);
     let args = parse_macro_input!(args as Literal);
-    unsafe {
-        I += 1;
-        let test_fn = format!("parser_test_{}_{}", ast.sig.ident.to_string(), I);
-        let test_fn = format_ident!("{}", test_fn);
-        let original_fn = format_ident!("{}", ast.sig.ident.to_string());
-        quote! {
-            #ast
-            #[test]
-            fn #test_fn() {
-                let arg = #args;
-                let span = Span::from(arg);
-                match #original_fn(span) {
-                    Err(e) => panic!("{:?}",e),
-                    Ok(span) => {
-                        if (span.0.len()!=0) {
-                            panic!("span is not empty get {:?}",span.0);
-                        }
-                    },
-                }
+    let mut hasher = DefaultHasher::new();
+    args.to_string().hash(&mut hasher);
+    let test_fn = format_ident!(
+        "parser_test_err_{}_{}",
+        ast.sig.ident.to_string(),
+        hasher.finish()
+    );
+    let original_fn = format_ident!("{}", ast.sig.ident.to_string());
+    quote! {
+        #ast
+        #[test]
+        fn #test_fn() {
+            let arg = #args;
+            let span = Span::from(arg);
+            match #original_fn(span) {
+                Err(e) => panic!("{:?}",e),
+                Ok(span) => {
+                    if (span.0.len()!=0) {
+                        panic!("span is not empty get {:?}",span.0);
+                    }
+                },
             }
         }
-        .into()
     }
+    .into()
 }
 #[proc_macro_attribute]
 pub fn test_parser_error(args: TokenStream, input: TokenStream) -> TokenStream {
-    static mut I: i32 = 0;
     let ast = parse_macro_input!(input as ItemFn);
-    unsafe {
-        I += 1;
-        let test_fn = format!("parser_test_error_{}_{}", ast.sig.ident.to_string(), I);
-        let test_fn = format_ident!("{}", test_fn);
-        let original_fn = format_ident!("{}", ast.sig.ident.to_string());
-        let args = args.to_string();
-        quote! {
-            #ast
-            #[test]
-            fn #test_fn() {
-                let arg = #args;
-                let span = Span::from(arg);
-                if let Ok(span) = #original_fn(span) {
-                    if (span.0.len()==0) {
-                        panic!("expected err but get ok , input is {:?}",arg);
-                    }
+    let args = parse_macro_input!(args as Literal);
+    let mut hasher = DefaultHasher::new();
+    args.to_string().hash(&mut hasher);
+    let test_fn = format_ident!(
+        "parser_test_err_{}_{}",
+        ast.sig.ident.to_string(),
+        hasher.finish()
+    );
+    let original_fn = format_ident!("{}", ast.sig.ident.to_string());
+    quote! {
+        #ast
+        #[test]
+        fn #test_fn() {
+            let arg = #args;
+            let span = Span::from(arg);
+            if let Ok(span) = #original_fn(span) {
+                if (span.0.len()==0) {
+                    panic!("expected err but get ok , input is {:?}",arg);
                 }
             }
         }
-        .into()
     }
+    .into()
 }
