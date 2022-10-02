cc6bca7cc8fdbccea4d122059a63c5818c71d886
bobxli <bobxli@tencent.com>
2022-10-03 01:08:09 +0800

    feat: struct init

--- src/ast/node/mod.rs
+++ src/ast/node/mod.rs
@@ -27,6 +27,7 @@ pub enum Value<'a> {
     VarValue(PointerValue<'a>),
     TypeValue(BasicTypeEnum<'a>),
     LoadValue(BasicValueEnum<'a>),
+    StructFieldValue((String, BasicValueEnum<'a>)),
     None,
 }
 
@@ -40,6 +41,7 @@ impl<'a> Value<'a> {
             Value::None => panic!("not implemented"),
             Value::TypeValue(_) => panic!("not implemented"),
             Value::LoadValue(v) => *v,
+            Value::StructFieldValue((_, v)) => *v,
         }
     }
 }
@@ -61,7 +63,7 @@ pub fn position_at_end<'a, 'b>(ctx: &mut Ctx<'b, 'a>, block: BasicBlock<'a>) {
 }
 
 pub fn alloc<'a, 'ctx>(
-    ctx: &mut Ctx<'a, 'ctx>,
+    ctx: &Ctx<'a, 'ctx>,
     tp: BasicTypeEnum<'ctx>,
     name: &str,
 ) -> PointerValue<'ctx> {
