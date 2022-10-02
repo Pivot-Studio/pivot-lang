695737fe55d5799dac1b514067500e79aa3c7e0d
bobxli <bobxli@tencent.com>
2022-10-03 03:34:21 +0800

    feat: static compile command

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,10 @@ struct Cli {
     #[clap(long)]
     genir: bool,
 
+    /// optimization level, 0-3
+    #[clap(short, value_parser, default_value = "0")]
+    optimization: u64,
+
     #[clap(subcommand)]
     command: Option<RunCommand>,
 }
@@ -42,15 +46,18 @@ enum RunCommand {
         /// Name of the compiled file
         #[clap(value_parser)]
         name: String,
-        /// optimization level, 0-3
-        #[clap(short, value_parser, default_value = "0")]
-        optimization: u64,
     },
 }
 
 fn main() {
     let cli = Cli::parse();
-
+    let opt = match cli.optimization {
+        0 => OptimizationLevel::None,
+        1 => OptimizationLevel::Less,
+        2 => OptimizationLevel::Default,
+        3 => OptimizationLevel::Aggressive,
+        _ => panic!("optimization level must be 0-3"),
+    };
     // You can check the value provided by positional arguments, or option arguments
     if let Some(name) = cli.name.as_deref() {
         let str = read_to_string(Path::new(name)).unwrap();
@@ -62,18 +69,12 @@ fn main() {
                 verbose: cli.verbose,
                 genir: cli.genir,
                 printast: cli.printast,
+                optimization: opt,
             },
         );
     } else if let Some(command) = cli.command {
         match command {
-            RunCommand::Run { name, optimization } => {
-                let opt = match optimization {
-                    0 => OptimizationLevel::None,
-                    1 => OptimizationLevel::Less,
-                    2 => OptimizationLevel::Default,
-                    3 => OptimizationLevel::Aggressive,
-                    _ => panic!("optimization level must be 0-3"),
-                };
+            RunCommand::Run { name } => {
                 Compiler::run(Path::new(name.as_str()), opt);
             }
         }
