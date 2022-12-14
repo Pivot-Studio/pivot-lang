e908a1cdef8995be57e5df9074e4bfaea74002b7
bobxli <bobxli@tencent.com>
2022-12-15 01:14:49 +0800

    refactor: fix salsa issue & refactor salsa codes fix: diagnostic terminal source

--- src/lsp/mem_docs.rs
+++ src/lsp/mem_docs.rs
@@ -1,6 +1,7 @@
 use std::{
     cell::RefCell,
     fs::read_to_string,
+    path::PathBuf,
     sync::{Arc, Mutex},
 };
 
@@ -12,7 +13,7 @@ use crate::{
         range::Pos,
     },
     nomparser::SourceProgram,
-    utils::read_config::Config,
+    utils::read_config::{get_config, get_config_path, Config},
     Db,
 };
 
@@ -20,7 +21,7 @@ use super::helpers::position_to_offset;
 
 #[derive(Debug, Clone)]
 pub struct MemDocs {
-    docs: FxHashMap<String, String>,
+    docs: FxHashMap<String, SourceProgram>,
 }
 
 #[salsa::tracked]
@@ -36,6 +37,17 @@ pub struct EmitParams {
 
 #[salsa::input]
 pub struct MemDocsInput {
+    pub docs: Arc<Mutex<RefCell<MemDocs>>>,
+    #[return_ref]
+    pub file: String,
+    pub op: Options,
+    pub action: ActionType,
+    pub params: Option<(Pos, Option<String>)>,
+    pub edit_pos: Option<Pos>,
+}
+
+#[salsa::input]
+pub struct MemDocsInputTracked {
     pub docs: Arc<Mutex<RefCell<MemDocs>>>,
     #[return_ref]
     pub file: String,
@@ -66,10 +78,10 @@ impl FileCompileInput {
             .docs(db)
             .lock()
             .unwrap()
-            .borrow()
-            .get_file_content(self.file(db));
+            .borrow_mut()
+            .get_file_content(db, self.file(db));
         if let Some(c) = re {
-            Some(SourceProgram::new(db, c, self.file(db).clone()))
+            Some(c)
         } else {
             None
         }
@@ -115,18 +127,52 @@ impl MemDocsInput {
             .docs(db)
             .lock()
             .unwrap()
-            .borrow()
-            .get_file_content(self.file(db));
+            .borrow_mut()
+            .get_file_content(db, self.file(db));
         if let Some(c) = re {
-            Some(SourceProgram::new(db, c, self.file(db).clone()))
+            Some(c)
         } else {
             None
         }
     }
-}
-
-pub fn get_doc_range(doc: &str, range: lsp_types::Range) -> String {
-    doc[position_to_offset(doc, range.start)..position_to_offset(doc, range.end)].to_string()
+    #[salsa::tracked(lru = 32)]
+    pub fn get_file_params(self, db: &dyn Db, f: String, entry: bool) -> Option<FileCompileInput> {
+        let mut file = f;
+        let path = get_config_path(file.clone());
+        if path.is_err() {
+            log::error!("lsp error: {}", path.err().unwrap());
+            return None;
+        }
+        let path = path.unwrap();
+        let buf = PathBuf::from(path.clone());
+        let parant = buf.parent().unwrap();
+        let re = get_config(
+            db,
+            self.docs(db)
+                .lock()
+                .unwrap()
+                .borrow_mut()
+                .get_file_content(db, &path)
+                .unwrap(),
+        );
+        if re.is_err() {
+            log::error!("lsp error: {}", re.err().unwrap());
+            return None;
+        }
+        let mut config = re.unwrap();
+        config.entry = parant.join(config.entry).to_str().unwrap().to_string();
+        if entry {
+            file = config.entry.clone();
+        }
+        config.root = parant.to_str().unwrap().to_string();
+        Some(FileCompileInput::new(
+            db,
+            file.clone(),
+            parant.to_str().unwrap().to_string(),
+            self,
+            config,
+        ))
+    }
 }
 
 impl MemDocs {
@@ -135,57 +181,78 @@ impl MemDocs {
             docs: FxHashMap::default(),
         }
     }
-    pub fn change(&mut self, range: lsp_types::Range, uri: String, text: String) {
+    pub fn change(&mut self, db: &mut dyn Db, range: lsp_types::Range, uri: String, text: String) {
         let doc = self.docs.get_mut(&uri.as_str().to_string()).unwrap();
-        doc.replace_range(
-            position_to_offset(doc, range.start)..position_to_offset(doc, range.end),
+        let mut txt = doc.text(db).clone();
+        txt.replace_range(
+            position_to_offset(&txt, range.start)..position_to_offset(&txt, range.end),
             &text,
         );
+        doc.set_text(db).to(txt);
     }
-    pub fn insert(&mut self, key: String, value: String) {
-        self.docs.insert(key, value);
+    pub fn insert(&mut self, db: &dyn Db, key: String, value: String, path: String) {
+        self.docs.insert(key, SourceProgram::new(db, value, path));
     }
-    pub fn get(&self, key: &str) -> Option<&String> {
+    pub fn get(&self, key: &str) -> Option<&SourceProgram> {
         self.docs.get(key)
     }
-    pub fn get_file_content(&self, key: &str) -> Option<String> {
+    pub fn get_file_content(&mut self, db: &dyn Db, key: &str) -> Option<SourceProgram> {
         let mem = self.get(key);
         if let Some(mem) = mem {
             return Some(mem.clone());
         }
         let re = read_to_string(key);
         if let Ok(re) = re {
-            return Some(re);
+            log::info!("read file from path{}", key);
+            self.insert(db, key.to_string(), re.clone(), key.to_string());
+            return self.get_file_content(db, key);
         }
         None
     }
-    pub fn get_mut(&mut self, key: &str) -> Option<&mut String> {
+    pub fn get_mut(&mut self, key: &str) -> Option<&mut SourceProgram> {
         self.docs.get_mut(key)
     }
-    pub fn remove(&mut self, key: &str) -> Option<String> {
+    pub fn remove(&mut self, key: &str) -> Option<SourceProgram> {
         self.docs.remove(key)
     }
-    pub fn iter(&self) -> impl Iterator<Item = &String> {
+    pub fn iter(&self) -> impl Iterator<Item = &SourceProgram> {
         self.docs.values()
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::db::Database;
+
     use super::*;
     use lsp_types::Position;
 
     #[test]
     fn test_mem_docs() {
+        let mut db = &mut Database::default();
         let mut mem_docs = MemDocs::new();
-        mem_docs.insert("test".to_string(), "test".to_string());
-        assert_eq!(mem_docs.get("test"), Some(&"test".to_string()));
-        assert_eq!(mem_docs.get_file_content("test"), Some("test".to_string()));
-        assert_eq!(mem_docs.get_file_content("test2"), None);
-        assert_eq!(mem_docs.remove("test"), Some("test".to_string()));
+        mem_docs.insert(
+            db,
+            "test".to_string(),
+            "test".to_string(),
+            "test".to_string(),
+        );
+        assert_eq!(mem_docs.get("test").unwrap().text(db), "test");
+        assert_eq!(
+            mem_docs.get_file_content(db, "test").unwrap().text(db),
+            "test"
+        );
+        assert_eq!(mem_docs.get_file_content(db, "test2"), None);
+        assert_eq!(mem_docs.remove("test").unwrap().text(db), "test");
         assert_eq!(mem_docs.get("test"), None);
-        mem_docs.insert("test".to_string(), "test".to_string());
+        mem_docs.insert(
+            db,
+            "test".to_string(),
+            "test".to_string(),
+            "test".to_string(),
+        );
         mem_docs.change(
+            db,
             lsp_types::Range {
                 start: Position {
                     line: 0,
@@ -199,8 +266,9 @@ mod tests {
             "test".to_string(),
             "哒哒哒".to_string(),
         );
-        assert_eq!(mem_docs.get("test"), Some(&"哒哒哒t".to_string()));
+        assert_eq!(mem_docs.get("test").unwrap().text(db), "哒哒哒t");
         mem_docs.change(
+            db,
             lsp_types::Range {
                 start: Position {
                     line: 0,
@@ -214,6 +282,6 @@ mod tests {
             "test".to_string(),
             "123".to_string(),
         );
-        assert_eq!(mem_docs.get("test"), Some(&"哒123哒t".to_string()));
+        assert_eq!(mem_docs.get("test").unwrap().text(db), "哒123哒t");
     }
 }
