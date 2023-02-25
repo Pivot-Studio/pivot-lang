9a802a5f033e7a3b67e20069bbf478f2eb1e5e05
bobxli <bobxli@tencent.com>
2023-02-25 23:58:17 +0800

    fix: merge queue

--- internal_macro/src/range_macro/src/lib.rs
+++ internal_macro/src/range_macro/src/lib.rs
@@ -6,18 +6,15 @@ pub fn range(_args: TokenStream, input: TokenStream) -> TokenStream {
     let mut ast = parse_macro_input!(input as DeriveInput);
     match &mut ast.data {
         syn::Data::Struct(ref mut struct_data) => {
-            match &mut struct_data.fields {
-                syn::Fields::Named(fields) => {
-                    fields.named.push(
-                        syn::Field::parse_named
-                            .parse2(quote! { pub range : crate::ast::range::Range })
-                            .unwrap(),
-                    );
-                }
-                _ => (),
+            if let syn::Fields::Named(fields) = &mut struct_data.fields {
+                fields.named.push(
+                    syn::Field::parse_named
+                        .parse2(quote! { pub range : crate::ast::range::Range })
+                        .unwrap(),
+                );
             }
             let ident = &ast.ident;
-            return quote! {
+            quote! {
                 #ast
                 impl crate::ast::node::RangeTrait for #ident {
                     fn range(&self) -> crate::ast::range::Range {
@@ -25,7 +22,7 @@ pub fn range(_args: TokenStream, input: TokenStream) -> TokenStream {
                     }
                 }
             }
-            .into();
+            .into()
         }
         _ => panic!("`range` has to be used with structs "),
     }
