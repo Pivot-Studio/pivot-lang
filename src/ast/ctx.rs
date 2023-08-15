3de6d88ef4e0a39833dfcaa20a8d0378901fcafb
Agent Smith <1769712655@qq.com>
2023-08-15 23:58:31 +0800

    fix: different tp instance in certain cond

--- src/ast/ctx.rs
+++ src/ast/ctx.rs
@@ -106,6 +106,7 @@ pub struct Ctx<'a> {
     pub generator_data: Option<Arc<RefCell<GeneratorCtxData>>>,
     pub generic_cache: GenericCache,
     pub origin_mod: *const Mod,
+    pub linked_tp_tbl: FxHashMap<*mut PLType, Vec<Arc<RefCell<PLType>>>>,
 }
 
 #[derive(Clone, Default)]
@@ -300,6 +301,7 @@ impl<'a, 'ctx> Ctx<'a> {
             generator_data: None,
             generic_cache: generic_infer,
             origin_mod: std::ptr::null(),
+            linked_tp_tbl: FxHashMap::default(),
         }
     }
     pub fn new_child(&'a self, start: Pos, builder: &'a BuilderEnum<'a, 'ctx>) -> Ctx<'a> {
@@ -339,6 +341,7 @@ impl<'a, 'ctx> Ctx<'a> {
             generator_data: self.generator_data.clone(),
             generic_cache: self.generic_cache.clone(),
             origin_mod: self.origin_mod,
+            linked_tp_tbl: FxHashMap::default(),
         };
         add_primitive_types(&mut ctx);
         if start != Default::default() {
@@ -1526,7 +1529,6 @@ impl<'a, 'ctx> Ctx<'a> {
                     need_up_cast: true,
                 };
             }
-            eprintln!("l:{:?}\nr:{:?}", trait_pltype, st_pltype);
             return EqRes {
                 eq: false,
                 need_up_cast: false,
