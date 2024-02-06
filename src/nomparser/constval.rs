cf77ad224ad5e587bcf6b7d10153b689fda903d4
CjiW <64598696+CjiW@users.noreply.github.com>
2024-02-06 10:08:15 +0800

    feat: hexadecimal & binary number (#389)
    * feat: hexadecimal & binary number
    
    * fix: clippy
    
    * feat: octal number
    
    * tests: add some tests

--- src/nomparser/constval.rs
+++ src/nomparser/constval.rs
@@ -1,5 +1,6 @@
 use nom::{
     branch::alt,
+    bytes::complete::tag,
     character::complete::{one_of, space0},
     combinator::{map_res, opt, recognize},
     multi::{many0, many1},
@@ -13,17 +14,38 @@ use internal_macro::{test_parser, test_parser_error};
 use nom::character::complete::char;
 
 use super::*;
-#[test_parser_error("_10")]
 #[test_parser(".10")]
 #[test_parser("10.10")]
 #[test_parser("10")]
-#[test_parser("10_00_3")]
+#[test_parser("10_00__3")]
+#[test_parser("0X1a")]
+#[test_parser("0o17")]
+#[test_parser("0b101")]
+#[test_parser_error("_10")]
 #[test_parser_error("10.")]
+#[test_parser_error("0x12g3")]
+#[test_parser_error("0o12.3")]
+#[test_parser_error("0b123")]
 pub fn number(input: Span) -> IResult<Span, Box<NodeEnum>> {
     let (input, _) = space0(input)?;
     let (re, value) = alt((
         map_res(float, |out| {
             Ok::<Num, ()>(Num::Float(out.fragment().parse::<f64>().unwrap()))
+        }),
+        map_res(hexadecimal, |out| {
+            let res =
+                u64::from_str_radix(out.fragment().replace('_', "").get(2..).unwrap(), 16).unwrap();
+            Ok::<Num, ()>(Num::Int(res))
+        }),
+        map_res(octal, |out| {
+            let res =
+                u64::from_str_radix(out.fragment().replace('_', "").get(2..).unwrap(), 8).unwrap();
+            Ok::<Num, ()>(Num::Int(res))
+        }),
+        map_res(binary, |out| {
+            let res =
+                u64::from_str_radix(out.fragment().replace('_', "").get(2..).unwrap(), 2).unwrap();
+            Ok::<Num, ()>(Num::Int(res))
         }),
         map_res(decimal, |out| {
             // TODO:err tolerate
@@ -71,6 +93,44 @@ pub fn bool_const(input: Span) -> IResult<Span, Box<NodeEnum>> {
 // the negative/postive mark will be treated as a unary operator
 fn decimal(input: Span) -> IResult<Span, Span> {
     recognize(many1(terminated(one_of("0123456789"), many0(char('_')))))(input)
+}
+
+#[test_parser("0x10")]
+#[test_parser("0X1a3")]
+#[test_parser("0x1_2__3")]
+#[test_parser_error("x123")]
+#[test_parser_error("0x12g3")]
+fn hexadecimal(input: Span) -> IResult<Span, Span> {
+    recognize(preceded(
+        alt((tag("0x"), tag("0X"))),
+        many1(terminated(
+            one_of("0123456789abcdefABCDEF"),
+            many0(char('_')),
+        )),
+    ))(input)
+}
+
+#[test_parser("0o10")]
+#[test_parser("0O173")]
+#[test_parser("0o1_2__3")]
+#[test_parser_error("o123")]
+#[test_parser_error("0o1283")]
+fn octal(input: Span) -> IResult<Span, Span> {
+    recognize(preceded(
+        alt((tag("0o"), tag("0O"))),
+        many1(terminated(one_of("01234567"), many0(char('_')))),
+    ))(input)
+}
+
+#[test_parser("0b101")]
+#[test_parser("0B0_01_")]
+#[test_parser_error("B001")]
+#[test_parser_error("0b0201")]
+fn binary(input: Span) -> IResult<Span, Span> {
+    recognize(preceded(
+        alt((tag("0b"), tag("0B"))),
+        many1(terminated(one_of("01"), many0(char('_')))),
+    ))(input)
 }
 
 #[test_parser(".12")]
