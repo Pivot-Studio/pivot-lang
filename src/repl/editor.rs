80c9d3f30fdbeede8649062a93a4e58f31fc4572
Agent Smith <mstmole@163.com>
2024-06-05 16:33:48 +0800

    feat: add headless repl mode to support jupyter notebook

--- src/repl/editor.rs
+++ src/repl/editor.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::{io::BufRead, path::Path, sync::atomic::AtomicBool};
 
 use rustyline::error::ReadlineError;
 
@@ -22,7 +22,6 @@ pub trait TermEditor {
         line: S,
     ) -> Result<bool, ReadlineError>;
 
-    #[cfg(test)]
     fn assert_err(&mut self, _err: bool) {
         // noop
     }
@@ -58,5 +57,62 @@ impl TermEditor for rustyline::Editor<REPLCompleter, rustyline::history::FileHis
         line: S,
     ) -> Result<bool, ReadlineError> {
         self.add_history_entry(line)
+    }
+}
+
+pub struct HeadlessEditor;
+
+static INIT: AtomicBool = AtomicBool::new(false);
+
+impl TermEditor for HeadlessEditor {
+    fn set_helper(&mut self, _helper: Option<REPLCompleter>) {}
+
+    fn readline(&mut self, _prompt: &str) -> Result<String, ReadlineError> {
+        if INIT.load(std::sync::atomic::Ordering::Relaxed) {
+            println!("@@@done@@@");
+            eprintln!("@@@done@@@");
+        }
+        INIT.store(true, std::sync::atomic::Ordering::Relaxed);
+        // 2K is a good default buffer size, but definitely do
+        // analyze the situation and adjust its size accordingly
+        let mut buffer = String::with_capacity(2048);
+        // Lock our standard input to eliminate synchronization overhead (unlocks when dropped)
+        let mut stdin = std::io::stdin().lock();
+
+        // Read our first line.
+        loop {
+            let start = buffer.len();
+            stdin.read_line(&mut buffer).expect("Failed to read line");
+            // read untile line "@@@done@@@"
+            if buffer.len() > start && buffer[start..].contains("@@@done@@@") {
+                buffer = buffer[..start].to_string();
+                break;
+            }
+        }
+        Ok(buffer)
+    }
+
+    fn load_history<P: AsRef<Path> + ?Sized>(&mut self, _path: &P) -> Result<(), ReadlineError> {
+        Ok(())
+    }
+
+    fn save_history<P: AsRef<Path> + ?Sized>(&mut self, _path: &P) -> Result<(), ReadlineError> {
+        Ok(())
+    }
+
+    fn append_history<P: AsRef<Path> + ?Sized>(&mut self, _path: &P) -> Result<(), ReadlineError> {
+        Ok(())
+    }
+
+    fn add_history_entry<S: AsRef<str> + Into<String>>(
+        &mut self,
+        _line: S,
+    ) -> Result<bool, ReadlineError> {
+        Ok(true)
+    }
+    fn assert_err(&mut self, err: bool) {
+        if err {
+            eprintln!("@@@error@@@");
+        }
     }
 }
