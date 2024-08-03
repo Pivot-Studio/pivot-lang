bdd75e8741a06d8b26af7d6b9a4f9543089c8bfa
Agent Smith <mstmole@163.com>
2024-08-03 12:08:44 +0800

    fix: Fix generator variable allocation bug
    The code changes in `src/ast/node/control.rs` fix a bug related to variable allocation in generator functions. Previously, when a variable was used in a generator, it needed to be stored in the context. However, there was an issue where the variable was not properly allocated in the context, leading to incorrect behavior.
    
    This commit addresses the bug by correctly allocating the variable in the context when inside a generator. It also adds a check to ensure that the allocation is only performed when necessary.
    
    I also added some code in iter test, to digging deeper into possibility of implement effect system based on generator.

--- src/ast/node/node_result.rs
+++ src/ast/node/node_result.rs
@@ -18,7 +18,7 @@ impl TerminatorEnum {
         self == TerminatorEnum::None || self == TerminatorEnum::YieldReturn
     }
     pub fn is_return(self) -> bool {
-        self == TerminatorEnum::Return || self == TerminatorEnum::YieldReturn
+        self == TerminatorEnum::Return
     }
 }
 
